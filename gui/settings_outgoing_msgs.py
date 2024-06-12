13c805a73b83d4572d7ad194f78d82c0c1f39653
Vertal <rec321@mail.ru>
2024-06-13 02:21:48 +0300

    Реализован модуль settings_outgoing_msgs.py - вкладка шаблонов исходящих писем и утилиты для запаиси этих шаблонов в базу.

--- gui/settings_outgoing_msgs.py
+++ gui/settings_outgoing_msgs.py
@@ -1,29 +1,185 @@
-from PyQt6.QtWidgets import QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QComboBox, QPushButton, QLineEdit, QTextEdit, QDialog, \
+    QDialogButtonBox, QMessageBox
 from base.base_tab import BaseTab
+from utils.utils_db import add_template_to_db, get_template_names, get_template_details, update_template_in_db, \
+    remove_template_from_db
+
 
 class MailerSettingsOutgoingMsgsTab(BaseTab):
     """
-    Класс для под-вкладки "Обучение" в "Настройки"
+    Класс для под-вкладки "Исходящие сообщения" в "Настройки"
     """
 
     def __init__(self, controller):
         """
-        Инициализация под-вкладки "Обучение"
+        Инициализация под-вкладки "Исходящие сообщения"
         :param controller: Экземпляр контроллера MainController
         """
         super().__init__(controller)
+
+        # Основной макет
         self.layout = QVBoxLayout()
-        self.label = QLabel(self.tr("Outgoin Msgs Content"))
-        self.layout.addWidget(self.label)
 
-        self.button = QPushButton(self.tr("Outgoin Msgs Button"))
-        self.layout.addWidget(self.button)
+        # Горизонтальный макет для метки, комбобокса и кнопок
+        self.horizontal_layout = QHBoxLayout()
 
+        # Метка "Template"
+        self.label = QLabel(self.tr("Template"))
+        self.horizontal_layout.addWidget(self.label)
+
+        # Комбобокс для выбора шаблонов
+        self.combo_box = QComboBox()
+        self.update_combo_box()
+        self.combo_box.currentIndexChanged.connect(self.load_template_details)
+        self.horizontal_layout.addWidget(self.combo_box)
+
+        # Кнопка "Добавить"
+        self.add_button = QPushButton(self.tr("Add"))
+        self.add_button.clicked.connect(self.open_add_template_dialog)
+        self.horizontal_layout.addWidget(self.add_button)
+
+        # Кнопка "Удалить"
+        self.remove_button = QPushButton(self.tr("Remove"))
+        self.remove_button.clicked.connect(self.remove_template)
+        self.horizontal_layout.addWidget(self.remove_button)
+
+        # Добавление горизонтального макета в основной макет
+        self.layout.addLayout(self.horizontal_layout)
+
+        # Текстовая строка "Sender"
+        self.sender_label = QLabel(self.tr("Sender"))
+        self.layout.addWidget(self.sender_label)
+
+        # Текстовое поле для отправителя
+        self.sender_edit = QLineEdit()
+        self.layout.addWidget(self.sender_edit)
+
+        # Текстовая строка "Subject"
+        self.subject_label = QLabel(self.tr("Subject"))
+        self.layout.addWidget(self.subject_label)
+
+        # Текстовое поле для заголовка
+        self.subject_edit = QLineEdit()
+        self.layout.addWidget(self.subject_edit)
+
+        # Многострочное текстовое поле "Content"
+        self.content_label = QLabel(self.tr("Content"))
+        self.layout.addWidget(self.content_label)
+
+        self.content_edit = QTextEdit()
+        self.layout.addWidget(self.content_edit)
+
+        # Кнопка "Save"
+        self.save_button = QPushButton(self.tr("Save"))
+        self.save_button.clicked.connect(self.save_template)
+        self.layout.addWidget(self.save_button)
+
+        # Установка основного макета
         self.setLayout(self.layout)
 
     def retranslate_ui(self):
         """
         Обновление текстов виджетов при смене языка
         """
-        self.label.setText(self.tr("Outgoin Msgs Content"))
-        self.button.setText(self.tr("Outgoin Msgs Button"))
+        self.label.setText(self.tr("Template"))
+        self.add_button.setText(self.tr("Add"))
+        self.remove_button.setText(self.tr("Remove"))
+        self.sender_label.setText(self.tr("Sender"))
+        self.subject_label.setText(self.tr("Subject"))
+        self.content_label.setText(self.tr("Content"))
+        self.save_button.setText(self.tr("Save"))
+
+    def update_combo_box(self):
+        """
+        Обновление содержимого комбобокса с шаблонами из базы данных
+        """
+        template_names = get_template_names()
+        self.combo_box.clear()
+        self.combo_box.addItems(template_names)
+
+    def open_add_template_dialog(self):
+        """
+        Открытие диалогового окна для добавления нового шаблона
+        """
+        dialog = QDialog()
+        dialog.setWindowTitle(self.tr("Add Template"))
+
+        dialog_layout = QVBoxLayout()
+        dialog.setLayout(dialog_layout)
+
+        label = QLabel(self.tr("Enter template name:"))
+        dialog_layout.addWidget(label)
+
+        self.template_name_edit = QLineEdit()
+        dialog_layout.addWidget(self.template_name_edit)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        button_box.accepted.connect(lambda: self.add_template(dialog))
+        button_box.rejected.connect(dialog.reject)
+        dialog_layout.addWidget(button_box)
+
+        dialog.exec()
+
+    def add_template(self, dialog):
+        """
+        Добавление нового шаблона в базу данных
+        :param dialog: Диалоговое окно для добавления шаблона
+        """
+        template_name = self.template_name_edit.text()
+        if template_name:
+            add_template_to_db(template_name)
+            self.update_combo_box()
+            dialog.accept()
+
+    def save_template(self):
+        """
+        Сохранение текущего шаблона в базу данных
+        """
+        template_name = self.combo_box.currentText()
+        if template_name:
+            sender = self.sender_edit.text()
+            subject = self.subject_edit.text()
+            content = self.content_edit.toPlainText()
+            update_template_in_db(template_name, sender, subject, content)
+        else:
+            self.show_no_template_dialog()
+
+    def show_no_template_dialog(self):
+        """
+        Показ диалогового окна при попытке сохранения без выбранного шаблона
+        """
+        QMessageBox.warning(self, self.tr("Warning"), self.tr("Please add and select a template before saving."))
+
+    def load_template_details(self):
+        """
+        Загрузка данных шаблона при выборе его из комбобокса
+        """
+        template_name = self.combo_box.currentText()
+        if template_name:
+            template_details = get_template_details(template_name)
+            if template_details:
+                self.sender_edit.setText(template_details[0])
+                self.subject_edit.setText(template_details[1])
+                self.content_edit.setText(template_details[2])
+            else:
+                self.sender_edit.clear()
+                self.subject_edit.clear()
+                self.content_edit.clear()
+
+    def remove_template(self):
+        """
+        Удаление выбранного шаблона из базы данных
+        """
+        template_name = self.combo_box.currentText()
+        if template_name:
+            reply = QMessageBox.question(self, self.tr("Remove Template"),
+                                         self.tr(f"Do you want to remove the template '{template_name}'?"),
+                                         QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            if reply == QMessageBox.StandardButton.Yes:
+                remove_template_from_db(template_name)
+                self.update_combo_box()
+                self.sender_edit.clear()
+                self.subject_edit.clear()
+                self.content_edit.clear()
+        else:
+            self.show_no_template_dialog()
--- utils/utils_db.py
+++ utils/utils_db.py
@@ -1,15 +1,13 @@
 """
 Создание и другие утилиты для работы с базой данных Почтовика
-:create_database(db_name) # Создание базы данных
+:create_database(db_name)
 """
-
 import sqlite3
 
 def create_database(db_name):
     """
     Создание базы данных Почтовика
-    :param db_name:
-    :return:
+    :param db_name: Имя файла базы данных
     """
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
@@ -20,7 +18,7 @@ def create_database(db_name):
                     DATETIME TEXT,
                     BILL_NUM TEXT,
                     ITEM TEXT,
-                    MSG_COINT INTEGER,
+                    MSG_COUNT INTEGER,
                     RECIPIENT_NAME TEXT,
                     RECIPIENT_EMAIL TEXT,
                     LAST_DATETIME TEXT,
@@ -73,5 +71,70 @@ def create_database(db_name):
                     CODE TEXT,
                     VALID_TO TEXT)''')
 
+    connection.commit()
+    connection.close()
+
+def add_template_to_db(template_name):
+    """
+    Добавление нового шаблона в таблицу MSG_TEMPLATES
+    :param template_name: Название шаблона
+    """
+    connection = sqlite3.connect('bd_mailer.db')
+    cursor = connection.cursor()
+    cursor.execute('''INSERT INTO MSG_TEMPLATES (NAME) VALUES (?)''', (template_name,))
+    connection.commit()
+    connection.close()
+
+def get_template_names():
+    """
+    Получение списка названий шаблонов из таблицы MSG_TEMPLATES
+    :return: Список названий шаблонов
+    """
+    connection = sqlite3.connect('bd_mailer.db')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT NAME FROM MSG_TEMPLATES''')
+    templates = cursor.fetchall()
+    connection.close()
+    return [template[0] for template in templates]
+
+def get_template_details(template_name):
+    """
+    Получение данных шаблона из таблицы MSG_TEMPLATES
+    :param template_name: Название шаблона
+
+
+    :return: Кортеж с данными шаблона (SENDER, SUBJECT, DATA)
+    """
+    connection = sqlite3.connect('bd_mailer.db')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT SENDER, SUBJECT, DATA FROM MSG_TEMPLATES WHERE NAME = ?''', (template_name,))
+    template_details = cursor.fetchone()
+    connection.close()
+    return template_details
+
+def update_template_in_db(template_name, sender, subject, content):
+    """
+    Обновление данных шаблона в таблице MSG_TEMPLATES
+    :param template_name: Название шаблона
+    :param sender: Отправитель
+    :param subject: Заголовок
+    :param content: Содержимое
+    """
+    connection = sqlite3.connect('bd_mailer.db')
+    cursor = connection.cursor()
+    cursor.execute('''UPDATE MSG_TEMPLATES
+                      SET SENDER = ?, SUBJECT = ?, DATA = ?
+                      WHERE NAME = ?''', (sender, subject, content, template_name))
+    connection.commit()
+    connection.close()
+
+def remove_template_from_db(template_name):
+    """
+    Удаление шаблона из таблицы MSG_TEMPLATES
+    :param template_name: Название шаблона
+    """
+    connection = sqlite3.connect('bd_mailer.db')
+    cursor = connection.cursor()
+    cursor.execute('''DELETE FROM MSG_TEMPLATES WHERE NAME = ?''', (template_name,))
     connection.commit()
     connection.close()
